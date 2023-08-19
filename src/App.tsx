a6a00d47391e247ebf9934cf7ed0674b3368f823
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-20 00:44:10 +0900

    add Card layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
-import { Box, Button, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Box,
+  Center,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+const Card: React.FC<{ title: string; description: string }> = ({
+  title,
+  description,
+}) => {
+  return (
+    <Box
+      bg="white"
+      p={5}
+      borderRadius="xl"
+      shadow="xl"
+      borderColor="gray.100"
+      borderWidth={2}
+      maxW="320px"
+    >
+      <AspectRatio ratio={16 / 9}>
+        <Image
+          src="https://via.placeholder.com/280"
+          borderRadius="lg"
+          objectFit="cover"
+          alt="Card image"
+        />
+      </AspectRatio>
+      <Heading size="lg" mt={4} mb={2}>
+        {title}
+      </Heading>
+      <Text color="gray.700">{description}</Text>
+    </Box>
+  );
+};
 
 const App = () => {
+  const cards = [
+    { title: "Card 1", description: "This is a description for card 1." },
+    { title: "Card 2", description: "This is a description for card 2." },
+    { title: "Card 3", description: "This is a description for card 3." },
+  ];
+
   return (
     <Box p={8} bgGradient="linear(to-r, teal.500,blue.500)" minHeight="100vh">
       <Stack
         direction="column"
         alignItems="center"
         justifyContent="center"
-        spacing={8}
+        spacing={10}
         minHeight="100vh"
       >
         <Image
@@ -20,10 +65,20 @@ const App = () => {
           Welcome to Chakra UI Demo
         </Heading>
         <Text fontSize="xl" color="white" textAlign="center" mb={6}>
-          A simple demonstration of Chakra UI's capabilities and elegance. Get
-          started with modern UI components!
+          A more sophisticated demonstration of Chakra UI's capabilities and
+          elegance.
         </Text>
-        <Button colorScheme="pink">Get Started</Button>
+        <Center>
+          <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} w="full">
+            {cards.map((card, index) => (
+              <Card
+                key={index}
+                title={card.title}
+                description={card.description}
+              />
+            ))}
+          </SimpleGrid>
+        </Center>
       </Stack>
     </Box>
   );
