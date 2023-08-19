81974232dfe9f904916a4bb10d8a8085361515e9
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-20 00:34:29 +0900

    add ChakraUI Top page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { Box, Button, Heading, Image, Stack, Text } from "@chakra-ui/react";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Box p={8} bgGradient="linear(to-r, teal.500,blue.500)" minHeight="100vh">
+      <Stack
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        spacing={8}
+        minHeight="100vh"
+      >
+        <Image
+          src="https://via.placeholder.com/150"
+          borderRadius="full"
+          boxSize={150}
+          alt="Logo"
+        />
+        <Heading color="white" mb={6}>
+          Welcome to Chakra UI Demo
+        </Heading>
+        <Text fontSize="xl" color="white" textAlign="center" mb={6}>
+          A simple demonstration of Chakra UI's capabilities and elegance. Get
+          started with modern UI components!
+        </Text>
+        <Button colorScheme="pink">Get Started</Button>
+      </Stack>
+    </Box>
+  );
 };
 
 export default App;
